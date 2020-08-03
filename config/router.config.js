bbd1811692d7cb43334e5111d0cafda7cd032f0a
pan1.zhang@changhong.com <pan1.zhang@changhong.com>
2020-08-03 09:57:11 +0800

    删除不需要的页面

--- config/router.config.js
+++ config/router.config.js
@@ -18,7 +18,6 @@ export default [
       { title: '元素库管理', path: '/elementLibrary', component: './ElementLibrary' },
       { title: '数据类型管理', path: '/DataType', component: './DataType' },
       { title: '数据源管理', path: '/dataSource', component: './DataSource' },
-      { title: '数据模型分类管理', path: '/dataModelType', component: './DataModelType' },
       { title: '数据模型管理', path: '/dataModel', component: './DataModelManager' },
       { title: '数据模型UI管理', path: '/dataModelUiConfig', component: './DataModelUiConfig' },
     ],
