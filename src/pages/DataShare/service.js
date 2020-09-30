cde144aa9e6f4d5b6d5717215f5aa41a85401b75
pan1.zhang@changhong.com <pan1.zhang@changhong.com>
2020-09-30 11:34:29 +0800

    更新

--- src/pages/DataShare/service.js
+++ src/pages/DataShare/service.js
@@ -53,7 +53,7 @@ export async function delChildRow(params) {
 
 /** 获取主数据字段 */
 export async function getPropertiesByCode({ code }) {
-  const url = `${MockServerPath}/masterDataUiConfig/getPropertiesByCode?code=${code}&random=${Math.random()}`;
+  const url = `${MockServerPath}/dataDefinition/getPropertiesByCode?code=${code}&random=${Math.random()}`;
   return request({
     url,
     method: 'GET',
