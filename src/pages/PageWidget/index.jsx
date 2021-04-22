1f74b97b87458fd62be7c85dca0bc02b98de50cb
pan1.zhang@changhong.com <pan1.zhang@changhong.com>
2021-04-22 16:38:23 +0800

    优化

--- src/pages/PageWidget/index.jsx
+++ src/pages/PageWidget/index.jsx
@@ -1,7 +1,9 @@
 import LedgerAccount from './LedgerAccount';
 import Personnel from './Personnel';
+import WBSProject from './WBSProject';
 
 export default {
   ledgerAccount: LedgerAccount,
   personnel: Personnel,
+  wbsProject: WBSProject,
 };
