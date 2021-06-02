4ba9480ef3b83be7ecbd16a8416c6b97d0a9dd39
pan1.zhang@changhong.com <pan1.zhang@changhong.com>
2021-06-02 09:59:20 +0800

    优化

--- src/components/ExtTablePreview/formatter.js
+++ src/components/ExtTablePreview/formatter.js
@@ -2,7 +2,7 @@ import React from 'react';
 import moment from 'moment';
 import { Tag } from 'antd';
 
-const formatDate = value => moment.format(value, 'YYYY-MM-DD');
+const formatDate = value => moment(value).format('YYYY-MM-DD');
 const formatText = value => value;
 const formatBool = value => <Tag color={value ? 'red' : 'green'}>{value ? '是' : '否'}</Tag>;
 
